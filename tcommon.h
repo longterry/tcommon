0a2cd1af9d480ddc7821be6b243123fac1898cd5
Terry <small.terry95@gmail.com>
2021-02-10 07:41:51 +0800

    add window type

--- tcommon.h
+++ tcommon.h
@@ -7,10 +7,18 @@ using tint16 = int16_t;
 using tint32 = int32_t;
 using tint64 = int64_t;
 
-using utint8  = unsigned char;
+#ifdef LINUX
+using utint8 = unsigned char;
 using utint16 = u_int16_t;
 using utint32 = u_int32_t;
 using utint64 = u_int64_t;
+using utint64 = u_int64_t;
+#elif WINDOWS
+using utint8 = unsigned char;
+using utint16 = uint16_t;
+using utint32 = uint32_t;
+using utint64 = uint64_t;
+#endif // DEBUG
 
 #define railog(...) printf("[INFO] "); printf(__VA_ARGS__); printf("\n");
 #define raiwarning(...)	printf("[Warning] "); printf(__VA_ARGS__); printf("\n");
