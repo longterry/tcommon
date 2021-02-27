12d3345a356a85180bc568158ec68517fd640f81
Terry <small.terry95@gmail.com>
2021-02-27 09:27:30 +0800

    [VCP-21] Add assert and returnlog

--- tcommon.h
+++ tcommon.h
@@ -2,7 +2,6 @@
 #define TCOMMON_H
 #include <string>
 #include <cstdint>
-
 using tint8  = char;
 using tint16 = int16_t;
 using tint32 = int32_t;
@@ -21,8 +20,26 @@ using utint32 = uint32_t;
 using utint64 = uint64_t;
 #endif // DEBUG
 
+#if (_WIN32||WIN64)
+#define __FILENAME__(x) strrchr(x,'\\')?strrchr(x,'\\')+1:x
+#define FILENAME __FILENAME__(__FILE__)
+#else
+#define __FILENAME__(x) strrchr(x,'/')?strrchr(x,'/')+1:x
+#define FILENAME __FILENAME__(__FILE__)
+#endif
+
+
 #define railog(...) printf("[INFO] "); printf(__VA_ARGS__); printf("\n");
 #define raiwarning(...)	printf("[Warning] "); printf(__VA_ARGS__); printf("\n");
-#define raierror(...) printf("[Error] "); printf(__VA_ARGS__); printf("\n");
+#define raierror(...)  printf("[Error][%s:%d][%s] ", FILENAME, __LINE__,__FUNCTION__); printf(__VA_ARGS__); printf("\n");
+
+#define returnlog(r,...) railog(__VA_ARGS__); return r;
+#define returnwarning(r,...) raiwarning(__VA_ARGS__); return r;
+#define returnerror(r,...) raierror(__VA_ARGS__); return r;
+
+#define DELETE_PTR(x)    if (nullptr != x) { delete x; x = nullptr; }
+#define ASSERT_EQUEL_RETURN(x, y, r)    if (y == x) { return r; }
+#define ASSERT_NOTEQUEL_RETURN(x, y, r)    if (y != x) { return r; }
+
 
 #endif // ! TCOMMON_H
